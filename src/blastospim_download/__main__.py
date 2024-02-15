bd4a6892076f504f99652a28b33bee3788be4a62
Abhishek Biswas <ab50@princeton.edu>
2024-02-15 16:39:19 -0500

    Fixed download function.

--- src/blastospim_download/__main__.py
+++ src/blastospim_download/__main__.py
@@ -4,26 +4,26 @@ import os
 import tarfile
 import requests, io
 
+def main():
+    ## Download early model
+    r = requests.get("https://blastospim.flatironinstitute.org/html/early_embryo_model.zip")
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall('.')
 
-## Download early model
-r = requests.get("https://blastospim.flatironinstitute.org/html/early_embryo_model.zip")
-z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall('.')
+    ## Download late model
+    r = requests.get("https://blastospim.flatironinstitute.org/html/late_blastocyst_model.zip")
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall('.')
 
-## Download late model
-r = requests.get("https://blastospim.flatironinstitute.org/html/late_blastocyst_model.zip")
-z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall('.')
-
-## Download the Test Data
-test_data_url = "https://drive.google.com/uc?id=1Vvzcj_Q_ERXsWqsbCfnk23wWdnz1vzWD"
-output_zip = 'toy_data_v1.0.tgz'
-gdown.download(test_data_url, output_zip, quiet=False)
-test_data_file = tarfile.open(output_zip)
-# extracting file
-test_data_file.extractall('.')
-test_data_file.close()
-os.remove(output_zip)
+    ## Download the Test Data
+    test_data_url = "https://drive.google.com/uc?id=1Vvzcj_Q_ERXsWqsbCfnk23wWdnz1vzWD"
+    output_zip = 'toy_data_v1.0.tgz'
+    gdown.download(test_data_url, output_zip, quiet=False)
+    test_data_file = tarfile.open(output_zip)
+    # extracting file
+    test_data_file.extractall('.')
+    test_data_file.close()
+    os.remove(output_zip)
 
 
 
